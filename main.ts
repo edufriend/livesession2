00a03b5f708cb4caac34f8ded5f8d9dea0d24b79
Your Name <m.awais.shaikh@gmail.com>
2024-04-04 00:58:19 +0500

    data types covered

---
+++ main.ts
@@ -0,0 +1,27 @@
+// let name1 = "Hello World";
+// console.log(name1)
+// name1 = "Ubaid Ali" 
+// console.log(name1);
+
+// var name1 = "variable by var"
+// var name1 = "Ubaid"
+// console.log(name1)
+
+
+// const name1 = "variable by const";
+// //name1 = "ubaid"
+// console.log(name1)
+
+
+// string is used when textual data is used
+// const name1 = "ubaid"    // infering type
+// const name2:string = "ubaid" // strongly typed
+// const num1 = 1000   // infering type
+// const num2:number = 1000   //strong typed
+// const news = false // infering type
+// const news1:boolean = true // strongly typed
+
+
+let anydata:any = 1000
+anydata = true   
+console.log(anydata) //
